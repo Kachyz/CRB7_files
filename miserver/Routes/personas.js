9537a0c49689d4408dd02e332d307b037069a383
Kachyz <kachiiz@gmail.com>
2019-02-28 21:50:39 -0600

    rutas

---
+++ miserver/Routes/personas.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const router = express.Router()
+const bodyParser = require('body-parser')
+
+router.use(bodyParser.json())
+
+let misPersonas = [{
+  nombre: 'Alan',
+  correo: 'alan@gmail.com'
+}, {
+  nombre: 'Jose',
+  correo: 'jose@gmail.com'
+}]
+
+router.get('/', (req, res) => {
+  console.log('Estoy en PERSONA');
+  res.send(misPersonas)
+})
+
+router.post('/', (req, res) => {
+  console.log(req.body)
+  misPersonas.push(req.body)
+  res.send(202)
+})
+
+router.delete('/:posicion', (req, res) => {
+  let posicionInicial = req.params.posicion
+  // misPersonas.splice(<Que posicion>, <Cuanto va a borrar = 1/>)
+  console.log(misPersonas);
+  misPersonas.splice(posicionInicial, 1)
+  console.log(misPersonas);
+  res.send("Elemento borrado")
+})
+
+router.put('/:posicion', (req, res) => {
+  let posicionInicial = req.params.posicion
+  // misPersonas.splice(<Que posicion>, <Cuanto va a borrar = 1/>)
+  console.log(misPersonas);
+  misPersonas[posicionInicial] = req.body
+  console.log(misPersonas);
+  res.send("Elemento actualizado")
+})
+
+// Params
+router.get('/:id', (req, res) => {
+  console.log(req);
+  res.send(misPersonas[req.params.id])
+})
+
+
+module.exports = router
--- miserver/app.js
+++ miserver/app.js
@@ -1,57 +1,16 @@
 const express = require('express') // express es el modulo
 let app = express() // app es el servidor 
 const bodyParser = require('body-parser')
+const rutaPersona = require('./Routes/personas')
 
 app.use(bodyParser.json())
+app.use('/individuo', rutaPersona)
 
-// let misPersonas = []
-let misPersonas = [{
-  nombre: 'Alan',
-  correo: 'alan@gmail.com'
-}, {
-  nombre: 'Jose',
-  correo: 'jose@gmail.com'
-}]
 
 // app.<metodo>(<ruta>, <cb>)
 app.get('/', (request, response) => {
   console.log('Hicieron una peticion GET a la raiz');
   response.send("<h1>Soy un servidor!!</h1>")
-})
-
-app.get('/persona', (req, res) => {
-  console.log('Estoy en PERSONA');
-  res.send(misPersonas)
-})
-
-app.post('/persona', (req, res) => {
-  console.log(req.body)
-  misPersonas.push(req.body)
-  res.send(202)
-})
-
-app.delete('/persona/:posicion', (req, res) => {
-  let posicionInicial = req.params.posicion
-  // misPersonas.splice(<Que posicion>, <Cuanto va a borrar = 1/>)
-  console.log(misPersonas);
-  misPersonas.splice(posicionInicial, 1)
-  console.log(misPersonas);
-  res.send("Elemento borrado")
-})
-
-app.put('/persona/:posicion', (req, res) => {
-  let posicionInicial = req.params.posicion
-  // misPersonas.splice(<Que posicion>, <Cuanto va a borrar = 1/>)
-  console.log(misPersonas);
-  misPersonas[posicionInicial] = req.body
-  console.log(misPersonas);
-  res.send("Elemento actualizado")
-})
-
-// Params
-app.get('/persona/:id', (req, res) => {
-  console.log(req);
-  res.send(misPersonas[req.params.id])
 })
 
 // Query
